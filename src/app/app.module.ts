0a0cc1bef1c362c2c0c96eda17d8ef0ed9e773b9
HiranSasitha <hiransasitha123@gamil.com>
2024-04-18 20:36:07 +0530

    completed user register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,24 @@ import {MatIcon, MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from "@angular/material/list";
 import { ContentComponent } from './component/content/content.component';
+import { HomeComponent } from './component/home/home.component';
+import { AdminComponent } from './component/admin/admin.component';
+import { UserComponent } from './component/user/user.component';
+import { LoginComponent } from './component/login/login.component';
+import { ForbiddenComponent } from './component/forbidden/forbidden.component';
+import { CarComponent } from './component/car/car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    HomeComponent,
+    AdminComponent,
+    UserComponent,
+    LoginComponent,
+    ForbiddenComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
